91227c7d9638090c80adf00c162db7dd9daeeadc
neighthin <nathan07@uw.edu>
2024-05-20 21:45:08 -0700

    Mobile Hamburger menu and CSS styling

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../index.css';
 
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    console.log("Menu toggled");
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="navbar">
       <div className="logo">
@@ -11,7 +17,10 @@ function Navbar() {
           <img src="img/logo.png" className="logo-icon" alt="Logo" />
         </NavLink>
       </div>
-      <div className="col text-left">
+      <button className="hamburger" onClick={toggleMenu}>
+        &#9776;
+      </button>
+      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
         <NavLink to="/quiz" className="btn"><div className="nav-text">Quiz</div></NavLink>
         <NavLink to="/diagram" className="btn"><div className="nav-text">Diagram</div></NavLink>
         <NavLink to="/resources" className="btn"><div className="nav-text">Resources</div></NavLink>
