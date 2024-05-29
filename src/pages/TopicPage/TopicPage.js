78972ace97c260906bec90fe1e702383c5321a33
JunnaC <junnacao@163.com>
2024-05-29 10:51:56 -0700

    resrouce accessibility update

--- src/pages/TopicPage/TopicPage.js
+++ src/pages/TopicPage/TopicPage.js
@@ -7,15 +7,15 @@ import '../../index.css';
 function TopicPage({ topic }) {
   const { topicid } = useParams();
 
-  if (!topic) return <div>Topic not found</div>;
+  if (!topic) return <div role="alert">Topic not found</div>;
 
   return (
     <div className="container">
       <ResourceNav topics={topicData} currentTopicId={topicid} className="sticky-nav" />
-      <h1>{topic.topicid}</h1>
+      <h1 id="topic-title">{topic.topicid}</h1>
       {topic.sections.map((section, idx) => (
-        <div key={idx}>
-          {section.title && <h2>{section.title}</h2>}
+        <div key={idx} aria-labelledby={`section-title-${idx}`}>
+          {section.title && <h2 id={`section-title-${idx}`}>{section.title}</h2>}
           <ul>
             {section.qa.map((qa, i) => (
               <li key={i}>
@@ -23,16 +23,15 @@ function TopicPage({ topic }) {
                 {qa.answerArr.map((answer, j) => (
                   <div key={j}>
                     {answer.type === 'youtube' && (
-                      <div>
-                        <p>{answer.name}</p>
-{answer.description && (
-  <div className="answer-description">
-    <p>{answer.description}</p>
-  </div>
-)}
-
+                      <div role="group" aria-labelledby={`video-title-${i}-${j}`}>
+                        <p id={`video-title-${i}-${j}`}>{answer.name}</p>
+                        {answer.description && (
+                          <div className="answer-description" aria-labelledby={`video-description-${i}-${j}`}>
+                            <p>{answer.description}</p>
+                          </div>
+                        )}
                         <iframe
-                          title="YouTube Video"
+                          title={`YouTube Video: ${answer.name}`}
                           width="497"
                           height="280"
                           src={`https://www.youtube.com/embed/${answer.videoId}`}
@@ -41,16 +40,19 @@ function TopicPage({ topic }) {
                         ></iframe>
                       </div>
                     )}
-                    {section.title === 'Anatomy' && answer.type === 'video' ? (
-                      <iframe
-                        title="Anatomy Video"
-                        width="497"
-                        height="280"
-                        src={answer.url}
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                      ></iframe>
-                    ) : null}
+                    {section.title === 'Anatomy' && answer.type === 'video' && (
+                      <div role="group" aria-labelledby={`anatomy-video-title-${i}-${j}`}>
+                        <p id={`anatomy-video-title-${i}-${j}`}>{answer.name}</p>
+                        <iframe
+                          title={`Anatomy Video: ${answer.name}`}
+                          width="497"
+                          height="280"
+                          src={answer.url}
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                          allowFullScreen
+                        ></iframe>
+                      </div>
+                    )}
                   </div>
                 ))}
               </li>
